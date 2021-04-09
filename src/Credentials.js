c3b11e31e008af82cc11980332f11f98a84635be
farmanalam285 <farmanalam285@gmail.com>
2021-04-09 23:18:11 +0530

    Done

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: 'bcc9e14e60474457a7d4db03531753ba',
+        ClientSecret: '4e44505a032949c68eb04803ba4385e3'
+    }
+}
+
+export { Credentials };
